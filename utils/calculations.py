e98c4aa8d404200680dd77782757645c603ffbae
farahnazabdi92 <139453837+farahnazabdi92@users.noreply.github.com>
2025-10-10 16:39:08 +0800

    chore(calc): add distance_clipped helper

--- utils/calculations.py
+++ utils/calculations.py
@@ -5,4 +5,7 @@ def sigmoid(z: float) -> float:
     z = float(z)
     return 1.0 / (1.0 + np.exp(-z))
 
-def distance(a, b): return float(np.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2))
+def distance_clipped(a, b):
+    return float(np.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2))
+
+
